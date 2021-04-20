8260bb7ff8083a372f0596688eb069ac12065ad1
Adrian Moldovan <adimoldovan28@gmail.com>
2021-04-20 23:17:16 +0300

    Update cw_event.tf

--- live/dev/cw_event.tf
+++ live/dev/cw_event.tf
@@ -4,7 +4,7 @@ module "cw_event" {
   awsRegion            = var.awsRegion
   environment          = var.environment
   cw_name              = "cw-event"
-  cw_timeadd           = "3m"
+  cw_timeadd           = "15m"
   cw_role              = module.iam.cw_role
   sfn_name             = module.sfn.sfn_name
   sfn_arn              = module.sfn.sfn_arn
